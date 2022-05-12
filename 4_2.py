9d35c0d3694023a263d234635d63f0397382247d
igormakhov <igor_makhov@yahoo.com>
2022-05-12 19:10:10 +0300

    uzhos

---
+++ 4_2.py
@@ -0,0 +1,11 @@
+spisok_1 = []
+text = input('Введи много слов ')
+spisok_1.extend(text)
+print({("a", spisok_1.count("a")), ("b", spisok_1.count("b")), ("c", spisok_1.count("c"))})#первый вариант, неправильный явно
+
+dlina_spiska = len(spisok_1)
+slovar = {}
+for n in range(0,dlina_spiska):
+    item = spisok_1[n]
+    slovar[item] = spisok_1.count(item)
+print(slovar)

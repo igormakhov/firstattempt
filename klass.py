260a22471c5189a7cb47f4f72022fdc3511d9a8e
igormakhov <igor_makhov@yahoo.com>
2022-05-13 13:05:24 +0300

    uzhos

--- klass.py
+++ klass.py
@@ -1,14 +1,27 @@
-stroka = input()
-spisok = []
-for unik in stroka:
-    symbol = stroka.count(unik)
-    if symbol == 1:
-        spisok.append(unik)
-print(spisok)
+# stroka = input()
+# spisok = []
+# for unik in stroka:
+#     symbol = stroka.count(unik)
+#     if symbol == 1:
+#         spisok.append(unik)
+# print(spisok)
+#
+# slovo = input("слово").lower().replace(' ', '')
+# if slovo == slovo[::-1]:
+#     print('Правда')
+# else:
+#     print("Херня")
+#     print("Совсем правда" if slovo == slovo[::-1] else "Совсем неправда")
 
-slovo = input("слово").lower().replace(' ', '')
-if slovo == slovo[::-1]:
-    print('Правда')
-else:
-    print("Херня")
-print("Совсем правда" if slovo == slovo[::-1] else "Совсем неправда")
+text = input().replace(' ','')
+glasnye = ("a", "e", "u", "i", "o")
+obshee = len(text)
+glas = 0
+for i in range(0, obshee):
+    if text[i] in glasnye:
+        glas = glas + 1
+neglasnye = obshee - glas
+print('Гласных', glas)
+print('Негласных', neglasnye)
+
+

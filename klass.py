2e6f8c3e500358fda5a0c1d9c10cdf0e5fec388b
igormakhov <igor_makhov@yahoo.com>
2022-05-13 12:47:28 +0300

    uzhos

--- klass.py
+++ klass.py
@@ -1,13 +1,14 @@
-# stroka = input()
-# spisok = []
-# for unik in stroka:
-#     symbol = stroka.count(unik)
-#     if symbol == 1:
-#         spisok.append(unik)
-# print(spisok)
+stroka = input()
+spisok = []
+for unik in stroka:
+    symbol = stroka.count(unik)
+    if symbol == 1:
+        spisok.append(unik)
+print(spisok)
 
 slovo = input("слово").lower().replace(' ', '')
 if slovo == slovo[::-1]:
     print('Правда')
 else:
     print("Херня")
+print("Совсем правда" if slovo == slovo[::-1] else "Совсем неправда")

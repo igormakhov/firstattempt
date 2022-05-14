997abfd4e06318c3b0618e54f021b3b0186552de
igormakhov <igor_makhov@yahoo.com>
2022-05-14 13:44:53 +0300

    uzhos

--- klass.py
+++ klass.py
@@ -1,27 +1,29 @@
-# stroka = input()
-# spisok = []
-# for unik in stroka:
-#     symbol = stroka.count(unik)
-#     if symbol == 1:
-#         spisok.append(unik)
-# print(spisok)
-#
-# slovo = input("слово").lower().replace(' ', '')
-# if slovo == slovo[::-1]:
-#     print('Правда')
-# else:
-#     print("Херня")
-#     print("Совсем правда" if slovo == slovo[::-1] else "Совсем неправда")
+stroka = input()
+spisok = []
+for unik in stroka:
+    symbol = stroka.count(unik)
+    if symbol == 1:
+        spisok.append(unik)
+print(spisok)
+
+slovo = input("слово").lower().replace(' ', '')
+if slovo == slovo[::-1]:
+    print('Правда')
+else:
+    print("Херня")
+    print("Совсем правда" if slovo == slovo[::-1] else "Совсем неправда")
 
 text = input().lower().replace(' ','')
 glasnye = ("a", "e", "u", "i", "o")
 obshee = len(text)
 glas = 0
+neglasnye = 0
 for i in range(0, obshee):
     if text[i].isalpha():# проверить чтобы убирало не буквы!!!!!!!!!!!!!!!!!!!
         if text[i] in glasnye:
             glas = glas + 1
-neglasnye = obshee - glas
+        else:
+            neglasnye = neglasnye + 1
 print('Гласных', glas)
 print('Негласных', neglasnye)
 

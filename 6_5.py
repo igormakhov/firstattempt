45f37e2ff126a943f08be6502854303168f70419
igormakhov <igor_makhov@yahoo.com>
2022-05-18 13:16:08 +0300

    uzhos

--- 6_5.py
+++ 6_5.py
@@ -1,9 +1,9 @@
-spisok = [1, 2, 3, 4, 5, 6]
-dlina = len(spisok)
-print(spisok[dlina-1])
+def perevorot(spisok):
+    dlina = len(spisok)
+    for a in range(dlina):
+        posled = spisok[dlina-1]
+        spisok.insert(a, posled)
+        del spisok[dlina]
+    print(spisok)
 
-for a in range(dlina):
-    posled = spisok[dlina-1]
-    spisok.insert(a, posled)
-    del spisok[dlina]
-print(spisok)
+perevorot([1, 2, 3, 4, 5, 6])

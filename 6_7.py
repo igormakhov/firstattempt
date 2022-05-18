01104f5fa4385d58c15297e2c96ef675b0cbf705
igormakhov <igor_makhov@yahoo.com>
2022-05-18 21:01:32 +0300

    uzhos

---
+++ 6_7.py
@@ -0,0 +1,10 @@
+spisok = [1, 3, 10, 5, 20, 1, 4, 15]
+dlina = len(spisok)
+for i in range(dlina-1):
+    if i < dlina and i != 0:
+        a = spisok[i] + spisok[i-1] + spisok[i+1]
+    elif i == 0:
+        a = spisok[i] + spisok[i+1] + spisok[dlina-1]
+    elif i == dlina:
+        a = spisok[i-1] + spisok[dlina-1] + spisok[0]
+    print(a)

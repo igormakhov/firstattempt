400659662fd31aeca9a4e97cbd721b2baddcbe39
igormakhov <igor_makhov@yahoo.com>
2022-05-19 11:53:24 +0300

    uzhos

--- 6_2.py
+++ 6_2.py
@@ -4,10 +4,9 @@ tablitsa_kodov = {'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.', 'f'
                   'y': '-.--', 'z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
                   '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.'}
 def perevod(text_1):
-    print(text_1)
-    perevod = []
+    perevod_1 = []
     for i in text_1:
-        perevod.append(tablitsa_kodov.get(i))#вставить проверку на пробелы и символы
-    print(perevod)
+        perevod_1.append(tablitsa_kodov.get(i))#убрать пробелы и символы - сделать проверку
+    print(perevod_1)
 
 perevod(input('введи текст на английском '))

468763d30388d152f79fd9a1df685f6b1d592b04
igormakhov <igor_makhov@yahoo.com>
2022-05-19 11:46:57 +0300

    uzhos

--- 6_4.py
+++ 6_4.py
@@ -1,9 +1,11 @@
 spisok = [1, 4, 'kvmek', 'ree', 234, '312', [14, 15, 17], (656, 452354, 'eter'), 'trt', 31]
-i = len(spisok)
-for a in range(i):
-    if not isinstance(spisok[a], str):
-        del spisok[a]
-        spisok.insert(a, '-')
-while '-' in spisok:
-    spisok.remove('-')
-print(spisok)
+def ost_stroki(text):
+    i = len(spisok)
+    for a in range(i):
+        if not isinstance(spisok[a], str):
+            del spisok[a]
+            spisok.insert(a, '-')
+    while '-' in spisok:
+        spisok.remove('-')
+    print(spisok)
+ost_stroki(spisok)

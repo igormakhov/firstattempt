8012cf3344b08999715b14a74962cef212e6918a
igormakhov <igor_makhov@yahoo.com>
2022-05-19 11:49:35 +0300

    uzhos

--- 6_7.py
+++ 6_7.py
@@ -1,10 +1,11 @@
 spisok = [1, 3, 10, 5, 20, 1, 4, 15]
-dlina = len(spisok)
-for i in range(dlina-1):
-    if i < dlina and i != 0:
-        a = spisok[i] + spisok[i-1] + spisok[i+1]
-    elif i == 0:
-        a = spisok[i] + spisok[i+1] + spisok[dlina-1]
-    elif i == dlina:
-        a = spisok[i-1] + spisok[dlina-1] + spisok[0]
-    print(a)
+def schyot_3(spis):
+    dlina = len(spisok)
+    for i in range(dlina-1):
+        if i < dlina and i != 0:
+            a = spisok[i] + spisok[i-1] + spisok[i+1]
+        elif i == 0:
+            a = spisok[i] + spisok[i+1] + spisok[dlina-1]
+        elif i == dlina:
+            a = spisok[i-1] + spisok[dlina-1] + spisok[0]
+schyot_3(spisok)

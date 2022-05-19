ba16f57709d4769b79f1c125f6da42beb0418b94
igormakhov <igor_makhov@yahoo.com>
2022-05-19 11:51:34 +0300

    uzhos

--- 6_8.py
+++ 6_8.py
@@ -1,8 +1,10 @@
 slovar = {'Ru': ('Mos','Spb', 'Kaz'), 'By': ('Min', 'Gro', 'Bre'), 'Lit': ('Vil', 'Kau', 'Pal')}
-i = input('Введи город ')
-a = slovar.keys()
-for b in a:
-    n = slovar[b]
-    for z in n:
-        if z == i:
-            print(b)
+def naidi_gorod(text):
+    i = input('Введи город ')
+    a = slovar.keys()
+    for b in a:
+        n = slovar[b]
+        for z in n:
+            if z == i:
+                print(b)
+naidi_gorod(slovar)

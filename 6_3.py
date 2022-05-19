878c8790f3ce064f60e4963c1ecb9eb697e05d77
igormakhov <igor_makhov@yahoo.com>
2022-05-19 11:45:28 +0300

    uzhos

--- 6_3.py
+++ 6_3.py
@@ -1,9 +1,10 @@
 spisok = [1, 2, 3, 4, 5, 6]
-n = int(input("введи смещение "))
-dlina = len(spisok)
-for a in range(n):
-    posled = spisok[dlina-1]
-    spisok.insert(0, posled)
-    del spisok[dlina]
-
-print(spisok)
+def sdvig_spiska(spis):
+    n = int(input("введи смещение "))
+    dlina = len(spisok)
+    for a in range(n):
+        posled = spisok[dlina-1]
+        spisok.insert(0, posled)
+        del spisok[dlina]
+    print(spisok)
+sdvig_spiska(spisok)

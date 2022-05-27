11dab5ef2b7efb553f9a33e036d2e6951ee845ef
igormakhov <igor_makhov@yahoo.com>
2022-05-27 08:16:21 +0300

    uzhos

--- 8_1.py
+++ 8_1.py
@@ -2,25 +2,26 @@ text = ['я запишу здесь кучу слов, они мне нужны 
         'большую кучу слов, а не маленькую. Совсем большую кучу. Они пригодятся. И не только мне.',
         'я буду писать из этих слов матрицу из слов. Будет ОЧень много слов.'
 ]
-stroka = str(text[1]).lower()
-spisok = list(stroka)
-for i in spisok:
-        if i.isalpha() or i == ' ':
-                continue
-        else:
-                spisok.remove(i)
-print(stroka)
-print(spisok)
-nov_spisok = f"{''.join(spisok)}"
-print(nov_spisok)
-sam_nov_spisok = nov_spisok.split()
-print(sam_nov_spisok)
 mega_nov_spisok = []
-dlina_nov = len(sam_nov_spisok)
-for a in range(0, dlina_nov):
-        if len(sam_nov_spisok[a]) > 2:
-               mega_nov_spisok.append(sam_nov_spisok[a])
-print(mega_nov_spisok)
+for s in range (0, len(text)):
+        stroka = str(text[s]).lower()
+        spisok = list(stroka)
+        for i in spisok:
+                if i.isalpha() or i == ' ':
+                        continue
+                else:
+                        spisok.remove(i)
+        # print(stroka)
+        # print(spisok)
+        nov_spisok = f"{''.join(spisok)}"
+        # print(nov_spisok)
+        sam_nov_spisok = nov_spisok.split()
+        # print(sam_nov_spisok)
+        dlina_nov = len(sam_nov_spisok)
+        for a in range(0, dlina_nov):
+                if len(sam_nov_spisok[a]) > 2:
+                        mega_nov_spisok.append(sam_nov_spisok[a])
+        # print(mega_nov_spisok)
 mega_dlina = len(mega_nov_spisok)
 slovar = {}
 for u in range(0, mega_dlina):
@@ -29,8 +30,3 @@ for u in range(0, mega_dlina):
         slovar[z] = d
 print(slovar)
 
-# slovar = {}
-# dlina = len(text)
-# for i in range(0, dlina):
-#         slovar[text[i]] = i
-# print(slovar)
